28400cde17cfb3edf1352abbacdb8e15da1e10d7
YinYang117 <slcryan2010@yahoo.com>
2022-05-11 14:23:03 -0600

    setting up

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,8 +5,8 @@ import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
+import UsersList from './components/auth/UsersList';
+import User from './components/auth/User';
 import { authenticate } from './store/session';
 
 function App() {
