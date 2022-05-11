8bf64ce10504fe4e860f9e87a25925f0750f7757
YinYang117 <slcryan2010@yahoo.com>
2022-05-11 11:22:09 -0600

    backend polish

---
+++ app/api/__init.py
@@ -0,0 +1,6 @@
+from .abiliby_routes import ability_routes
+from .hero_routes import hero_routes
+from .hero_abil_routes import hero_abli_routes
+from .auth_routes import auth_routes
+from .user_routes import user_routes
+
