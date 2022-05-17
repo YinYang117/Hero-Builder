1dee70d277706976f734b9327546309deadce76a
YinYang117 <slcryan2010@yahoo.com>
2022-05-17 08:31:13 -0600

    added button on home page to go to home

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,12 +1,25 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom"
 import seige from "./seige.jpg"
 import './splash.css'
 
 
 const Splash = () => {
+    const history = useHistory();
+	const user = useSelector(state => state.session.user)
+
+    const toHome = () => {
+        history.push("/home")
+    }
 
     return (
+    <>
         <img src={seige} alt="Splash Seige" className="splash" />
-
+        {user &&
+            <button className="toHome" onClick={toHome}>Start Building Heros!</button>
+        }
+    </>
     )
 }
 
