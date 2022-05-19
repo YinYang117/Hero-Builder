1abc97172529259ffb4393a23635555828c2dfa8
YinYang117 <slcryan2010@yahoo.com>
2022-05-19 12:35:03 -0600

    basic form for adding new abilities

--- react-app/src/components/Abil/newAbil.js
+++ react-app/src/components/Abil/newAbil.js
@@ -15,15 +15,15 @@ const NewAbilCard = (         ) => {
 	const [description, setDescription] = useState("")
 	const [abilityImage, setAbilityImage] = useState("https://res.cloudinary.com/dzrimpg5t/image/upload/v1652916118/thorn_pre75p.png")
 	const [usesResource, setUsesResource] = useState(0)
-	const [resourceName, setResourceName] = useState()
-	const [resourceCost, setResourceCost] = useState()
+	const [resourceName, setResourceName] = useState("")
+	const [resourceCost, setResourceCost] = useState(0)
 	const [usesCharges, setUsesCharges] = useState(0)
-	const [numCharges, setNumCharges] = useState()
-	const [chargeRechargeRate, setChargeRechargeRate] = useState()
+	const [numCharges, setNumCharges] = useState(0)
+	const [chargeRechargeRate, setChargeRechargeRate] = useState(0)
 	const [usesCooldown, setUsesCooldown] = useState(0)
-	const [cooldown, setCooldown] = useState()
+	const [cooldown, setCooldown] = useState(0)
 	const [channeled, setChanneled] = useState(0)
-	const [channelTime, setChannelTime] = useState()
+	const [channelTime, setChannelTime] = useState(0)
 	const [ultimate, setUltimate] = useState(0)
 	const [details, setDetails] = useState("")
 
@@ -72,16 +72,17 @@ const NewAbilCard = (         ) => {
 		newAbil.details = details
 
 		const data = await dispatch(buildAbil(newAbil))
-		if (!data) return "TODO make it stop building new"
+		if (!data) return // TODO toggle build state
 		else setErrors(data) // should be an array
 	}
 
-	
 	// const handleCancel = () => {
 	// 	// TODO clear fields?
 	// 	setBuildNewAbil(false)
 	// }
 
+	// TODO setAbilityImage() based off click
+	
 	return (
 		// whole container is col
 		<div className="fdcol">
@@ -93,6 +94,134 @@ const NewAbilCard = (         ) => {
 				e.preventDefault()
 				submitNewAbil()
 			}}>
+				<div className="left side fdcol">
+					<img src={abilityImage} alt="new ability portraite" className="heroDetImg" /> {/* TODO classname */}
+					<div className="conditionGroup fdcol">
+						<div>Uses Resources?</div>
+						<div className="checkboxContainer fdrow">
+							<input type="checkbox" name="" checked={ === 1} value={1} onClick={e => (e)} />
+							<label>Yes</label>
+							<input type="checkbox" name="" checked={ === 0} value={0} onClick={e => (e)} />
+							<label>No</label>
+						</div>
+					</div>
+					<div className="conditionGroup fdcol">
+						<div>Uses Charges?</div>
+						<div className="checkboxContainer fdrow">
+							<input type="checkbox" name="" checked={ === 1} value={1} onClick={e => (e)} />
+							<label>Yes</label>
+							<input type="checkbox" name="" checked={ === 0} value={0} onClick={e => (e)} />
+							<label>No</label>
+						</div>
+					</div>
+					<div className="conditionGroup fdcol">
+						<div>Uses Cooldowns?</div>
+						<div className="checkboxContainer fdrow">
+							<input type="checkbox" name="" checked={ === 1} value={1} onClick={e => (e)} />
+							<label>Yes</label>
+							<input type="checkbox" name="" checked={ === 0} value={0} onClick={e => (e)} />
+							<label>No</label>
+						</div>
+					</div>
+					<div className="conditionGroup fdcol">
+						<div>Channeled While Casting?</div>
+						<div className="checkboxContainer fdrow">
+							<input type="checkbox" name="" checked={ === 1} value={1} onClick={e => (e)} />
+							<label>Yes</label>
+							<input type="checkbox" name="" checked={ === 0} value={0} onClick={e => (e)} />
+							<label>No</label>
+						</div>
+					</div>
+					<div className="conditionGroup fdcol">
+						<div>Ultimate Ability?</div>
+						<div className="checkboxContainer fdrow">
+							<input type="checkbox" name="" checked={ === 1} value={1} onClick={e => (e)} />
+							<label>Yes</label>
+							<input type="checkbox" name="" checked={ === 0} value={0} onClick={e => (e)} />
+							<label>No</label>
+						</div>
+					</div>
+				</div>
+				<div className="right side fdcol">
+					<>
+						<label className="mlr10" >Name:</label>
+						<input onChange={e => (e.target.value)}
+							className="w100p"
+							type="text"
+							placeholder='Ability Name'
+							required="required"
+							value={name} />
+					</>
+					<>
+						<label>Description:</label>
+						<textarea onChange={e => setIntro(e.target.value)}
+							className="hauto wauto editIntro"
+							id="editIntro"
+							rows="6" cols="35"
+							placeholder='General Description'
+							required="required"
+							value={intro} />
+					</>
+					<div className="conditionDetails1 fdrow sb">
+						<div className="fdcol">
+							<label>Resource Name</label>
+							<input onChange={e => (e.target.value)}
+								className=""
+								type=""
+								placeholder=''
+								required="required"
+								value={} />
+						</div>
+						<div className="fdcol">
+							<label>Resource Amount</label>
+							<input onChange={e => (e.target.value)}
+								className=""
+								type=""
+								placeholder=''
+								required="required"
+								value={} />
+						</div>
+					</div>
+					<div className="conditionDetails2 fdrow sb">
+						<div className="fdcol">
+							<label>Number of Charges</label>
+							<input onChange={e => (e.target.value)}
+								className=""
+								type=""
+								placeholder=''
+								required="required"
+								value={} />
+						</div>
+						<div className="fdcol">
+							<label>Charge Replenish Rate</label>
+							<input onChange={e => (e.target.value)}
+								className=""
+								type=""
+								placeholder=''
+								required="required"
+								value={} />
+						</div>
+					</div>
+					<div className="conditionDetails1 fdrow sa">
+						<label>Cooldown Time</label>
+						<input onChange={e => (e.target.value)}
+							className=""
+							type=""
+							placeholder=''
+							required="required"
+							value={} />
+					</div>
+					<div className="conditionDetails2 fdrow sa">
+						<label>Channel Duration</label>
+						<input onChange={e => (e.target.value)}
+							className=""
+							type=""
+							placeholder=''
+							required="required"
+							value={} />
+					</div>
+					
+				</div>
 
 
 
