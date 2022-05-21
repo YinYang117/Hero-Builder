8b8180853979fef26b2034cf9225d1882eab92d7
YinYang117 <slcryan2010@yahoo.com>
2022-05-20 19:30:14 -0600

    display abil details better

--- react-app/src/components/Abil/abilDetails.js
+++ react-app/src/components/Abil/abilDetails.js
@@ -20,8 +20,29 @@ const AbilDetails = ({setEditingAbil}) => {
 
 	const usesResourcesFunc = () => {
 		if (currAbil.usesResource === 1) return (<div>Yes</div>)
-		else return (<div>No</div>) // (currAbil.resource === 0)
+		else return (<div>No</div>) // === 0
 	}
+
+	const usesChargesFunc = () => {
+		if (currAbil.usesCharges === 1) return (<div>Yes</div>)
+		else return (<div>No</div>) // === 0
+	}
+
+	const usesCooldownFunc = () => {
+		if (currAbil.usesCooldown === 1) return (<div>Yes</div>)
+		else return (<div>No</div>) // === 0
+	}
+
+	const channeledFunc = () => {
+		if (currAbil.channeled === 1) return (<div>Yes</div>)
+		else return (<div>No</div>) // === 0
+	}
+
+	const ultimateFunc = () => {
+		if (currAbil.ultimate === 1) return (<div>Yes</div>)
+		else return (<div>No</div>) // === 0
+	}
+	
 
 	return (
 		<>
@@ -30,66 +51,75 @@ const AbilDetails = ({setEditingAbil}) => {
 				{/* left, image + name */}
 				<div className="fdcol hfmn" >
 					<div className="fdrow sb" >
-						<div className="mlr10 w100p aicen heroDetName p5" >Name: {currAbil.name}</div>
+						<div className="mlr10 w100p aicen abilDetName p5" >Name: {currAbil.name}</div>
 					</div>
-					<img src={currAbil.abilImage} alt={currAbil.name} className="heroDetImg" />
+					<img src={currAbil.abilityImage} alt={currAbil.name} className="abilDetImg" />
 				</div>
 				{/* right, main data */}
 				<div className="fdcol hfmn" >
 					<div className="dataStripe1 fdrow sa aicen" >
-						<button onClick={e => editAbilFunc()}
+						<button onClick={editAbilFunc}
 							className="w40p h80p confirmShadow"
 						>
-							Edit</button>
+							Edit
+						</button>
 						<button onClick={deleteAbilFunc}
 							className="w40p h80p cancelShadow"
 						>
-							Delete</button>
+							Delete
+						</button>
 					</div>
 					<div className="dataStripe2 fdrow sa aicen" >
-						<div className="mlr10">HP:</div>
 						<div className="mlr10 fdcol aicen">
 							<div>Use Resources?</div>
 							<div>{usesResourcesFunc}</div>
 						</div>
 					</div>
+					{(currAbil.usesRerources === 1) &&
 					<div className="dataStripe3 fdrow sa aicen" >
-						<div className="mlr10 fdcoln">resource name: </div>
-						<div className="mlr10 fdcol">resource cost</div>
-					</div>
-					<div className="dataStripe1 fdrow sa aicen" >
-						<div className="mlr10 fdrow aicen">
-							<div>Physical Armor: </div>
-							<div>amt</div>
-						</div>
-						<div className="mlr10 fdrow aicen">
-							<div>Magic Resist: </div>
-							<div>amt</div>
+						<div className="mlr10">Resource Name: {currAbil.resourceName}</div>
+						<div className="mlr10">Resource Cost: {currAbil.resourceCost}</div>
+					</div>}
+					<div className="dataStripe2 fdrow sa aicen" >
+						<div className="mlr10 fdcol aicen">
+							<div>Use Charges?</div>
+							<div>{usesChargesFunc}</div>
 						</div>
 					</div>
+					{(currAbil.usesCharges === 1) &&
+					<div className="dataStripe3 fdrow sa aicen" >
+						<div className="mlr10 fdcoln">Number of Charges: {currAbil.numCharges}</div>
+						<div className="mlr10 fdcol">resource cost: {currAbil.chargeRechargeRate}</div>
+					</div>}
 					<div className="dataStripe2 fdrow sa aicen" >
 						<div className="mlr10 fdrow aicen">
-							<div>Attack Damage: </div>
-							<div>da</div>
+							<div>Uses Cooldown?</div>
+							<div>{usesCooldownFunc}</div>
 						</div>
+						{(currAbil.usesCooldown) &&
 						<div className="mlr10 fdrow aicen">
-							<div>Attack Range: </div>
-							<div>at</div>
-						</div>
+							<div>Cooldown Time: {currAbil.cooldown}</div>
+						</div>}
 					</div>
 					<div className="dataStripe1 fdrow sa aicen" >
 						<div className="fdrow sa aicen">
-							<div>Attack Speed: </div>
-							<div className="mlr10">sd</div>
+							<div>Channeled Ability?</div>
+							<div>{channeledFunc}</div>
 						</div>
+						{(currAbil.channeled) &&
 						<div className="fdrow aicen">
-							<div>Move Speed: </div>
-							<div className="mlr10">ms</div>
+							<div>Channel Time: {currAbil.channelTime}</div>
+						</div>}
+					</div>
+					<div className="dataStripe1 fdrow sa aicen" >
+						<div className="fdrow sa aicen">
+							<div>Ultimate Ability?</div>
+							<div>{ultimateFunc}</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			{currAbil.details && <div className='heroIntro p5'>
+			{currAbil.details && <div className='abilIntro p5'>
 				{currAbil.details}
 			</div>}
 		</>
